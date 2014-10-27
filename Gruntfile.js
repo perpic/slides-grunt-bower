c6dcbc1004812e86a49f9fe67d956bc9f541ee09
Francesc Perez <f.perez.picazo@gmail.com>
2014-10-27 20:32:58 +0000

    Add concat and uglify grunt plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,31 +8,41 @@ module.exports = function(grunt) {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      slides: {
+      slidesJs: {
         src: [
           'components/reveal.js/lib/js/head.min.js',
           'components/reveal.js/js/reveal.min.js'
         ],
         dest: 'src/js/slides.min.js'
       },
-      markdown: {
+      slidesCss: {
+        src: [
+          'components/reveal.js/css/theme/night.css',
+          'components/reveal.js/lib/css/zenburn.css'
+        ],
+        dest: 'src/css/slides.css'
+      }
+    },
+
+    uglify: {
+      options: {
+        preserveComments: 'some'
+      },
+      markdownJs: {
         src: [
           'components/reveal.js/plugin/markdown/marked.js',
           'components/reveal.js/plugin/markdown/markdown.js'
         ],
-        dest: 'src/js/markdown.js'
-      },
-      highlight: {
-        src: 'components/reveal.js/plugin/highlight/highlight.js',
-        dest: 'src/js/highlight.js'
+        dest: 'src/js/markdown.min.js'
       }
     }
   });
 
   // Grunt plugins and tasks
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Custom tasks
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat', 'uglify']);
 
 };
